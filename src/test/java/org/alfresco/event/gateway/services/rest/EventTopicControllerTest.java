d90db62b38499551c738e5549404b29a2bf75548
Jared Ottley <jared.ottley@alfresco.com>
2018-09-25 08:36:25 -0600

    [EGATE-91] EventTopicControllerTest not finding application.properties when run with other tests -Add TestPropertySource annotation

--- src/test/java/org/alfresco/event/gateway/services/rest/EventTopicControllerTest.java
+++ src/test/java/org/alfresco/event/gateway/services/rest/EventTopicControllerTest.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:application.properties")
 public class EventTopicControllerTest
 {
     @Autowired
